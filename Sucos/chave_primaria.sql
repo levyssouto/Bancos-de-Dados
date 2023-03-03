d3caa62db266dddf8686ea6ce9c255042bdfda4b
levyssouto <levy.soutosousa@gmail.com>
2023-03-03 17:01:56 -0300

    adicionando chave primária

---
+++ Sucos/chave_primaria.sql
@@ -0,0 +1,12 @@
+/*Criando chaves primárias - não deixa criar produtos repetidos com mesmo código*/
+ALTER TABLE tbprodutos ADD PRIMARY KEY (PRODUTO);
+
+select * from tbprodutos;
+
+INSERT INTO tbprodutos (
+PRODUTO,  NOME, EMBALAGEM, TAMANHO, SABOR,
+PRECO) VALUES
+('1078680', 'Frescor do Verão - 470 ml - Manga', 'Lata', '470 ml','Manga',5.18);
+
+UPDATE TBPRODUTOS SET EMBALAGEM = 'Garrafa', PRECO = 2.46
+WHERE PRODUTO = '1078680';
