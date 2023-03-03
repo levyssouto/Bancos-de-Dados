bf44a0bc0a59c4e81e6be628f5f79e65a96937ed
levyssouto <levy.soutosousa@gmail.com>
2023-03-03 16:27:09 -0300

    mudança na tabla de vendedores

--- Sucos/excluindo_regitsros.sql
+++ Sucos/excluindo_regitsros.sql
@@ -1,3 +1,8 @@
 /*Excluindo Registros*/
 DELETE FROM tbprodutos WHERE PRODUTO = '1078680';
+
 select * from tbprodutos;
+
+DELETE FROM tabela_de_vendedores WHERE MATRICULA = '00233';
+
+select * from tabela_de_vendedores; 
