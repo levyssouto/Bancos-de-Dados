d21c1753ee9b26306ef975844b2ea87018221b47
levyssouto <levy.soutosousa@gmail.com>
2023-03-03 11:57:03 -0300

    novo arquivo de inserir produtos utilizando insert

---
+++ inserir_produtos.sql
@@ -0,0 +1,11 @@
+
+
+INSERT INTO tbprodutos (
+PRODUTO,  NOME, EMBALAGEM, TAMANHO, SABOR,
+PRECO) VALUES
+('544931', 'Frescor do Verão - 350 ml - Limão', 'PET', '350 ml','Limão',3.20);
+
+INSERT INTO tbprodutos (
+PRODUTO,  NOME, EMBALAGEM, TAMANHO, SABOR,
+PRECO) VALUES
+('1078680', 'Frescor do Verão - 470 ml - Manga', 'Lata', '470 ml','Manga',5.18);
